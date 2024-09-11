c312e135cd938d85c7d3780bc95f1ae8a54bcd7c
AleenaKhan10 <aleenakhanraees40@gmail.com>
2024-09-11 15:37:22 +0500

    updated the token and default settings.

--- v1/config.py
+++ v1/config.py
@@ -11,11 +11,11 @@ from datetime import timedelta
     # REMEMBER_COOKIE_DURATION = timedelta(seconds=20)
         
 
-# class Config:
-#     SECRET_KEY = "0332033603250309"  # You should set a secret key here
-#     SQLALCHEMY_DATABASE_URI = (
-#         "mysql+pymysql://root:root@localhost:3306/agency_apidb"
-#     )
-#     SQLALCHEMY_TRACK_MODIFICATIONS = False
-#     SESSION_COOKIE_SECURE = True
-#     REMEMBER_COOKIE_DURATION = timedelta(seconds=20)
+class Config:
+    SECRET_KEY = "0332033603250309"  # You should set a secret key here
+    SQLALCHEMY_DATABASE_URI = (
+        "mysql+pymysql://root:root@localhost:3306/agency_apidb"
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SESSION_COOKIE_SECURE = True
+    REMEMBER_COOKIE_DURATION = timedelta(seconds=20)
